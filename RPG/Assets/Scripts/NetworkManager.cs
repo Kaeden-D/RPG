e00ae6beee376b2a765a003b6549cc3f12f154ff
Kaeden-D <dalyk3@my.erau.edu>
2025-10-06 10:47:03 -0700

    Create project and add Network manager
    Need to do tile maps on page 9 and continue from page 18

---
+++ RPG/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,66 @@
+using NUnit;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    public int maxPlayers;
+
+    // instance
+    public static NetworkManager instance;
+    void Awake()
+    {
+
+        if (instance != null && instance != this)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+    void Start()
+    {
+
+        // connect to the master server
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+
+        PhotonNetwork.JoinLobby();
+
+    }
+
+    public void CreateRoom(string roomName)
+    {
+
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = (byte)maxPlayers;
+        PhotonNetwork.CreateRoom(roomName, options);
+
+    }
+
+    public void JoinRoom(string roomName)
+    {
+
+        PhotonNetwork.JoinRoom(roomName);
+
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+
+        PhotonNetwork.LoadLevel(sceneName);
+
+    }
+
+}
